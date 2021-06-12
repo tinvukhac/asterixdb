41cd1bbdf81b94006c7ce8b804dcfacb1075f608
Tin Vu <tinvukhac@gmail.com>
2021-06-12 16:29:20 -0700

    Refactor STFunctions
    Change-Id: I71636c1abf4a2b9fa1a537109d45b1c234574296

--- asterixdb/asterix-geo/src/main/java/org/apache/asterix/geo/evaluators/functions/AbstractSTGeometryDoubleNDescriptor.java
+++ asterixdb/asterix-geo/src/main/java/org/apache/asterix/geo/evaluators/functions/AbstractSTGeometryDoubleNDescriptor.java
@@ -26,14 +26,17 @@ import org.apache.asterix.dataflow.data.nontagged.serde.ADoubleSerializerDeseria
 import org.apache.asterix.dataflow.data.nontagged.serde.AGeometrySerializerDeserializer;
 import org.apache.asterix.dataflow.data.nontagged.serde.AInt64SerializerDeserializer;
 import org.apache.asterix.formats.nontagged.SerializerDeserializerProvider;
+import org.apache.asterix.om.base.ARectangle;
 import org.apache.asterix.om.types.ATypeTag;
 import org.apache.asterix.om.types.BuiltinType;
 import org.apache.asterix.runtime.evaluators.base.AbstractScalarFunctionDynamicDescriptor;
+import org.apache.asterix.runtime.evaluators.functions.PointableHelper;
 import org.apache.asterix.runtime.exceptions.InvalidDataFormatException;
 import org.apache.asterix.runtime.exceptions.TypeMismatchException;
 import org.apache.hyracks.algebricks.runtime.base.IEvaluatorContext;
 import org.apache.hyracks.algebricks.runtime.base.IScalarEvaluator;
 import org.apache.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
+import org.apache.hyracks.api.dataflow.value.ISerializerDeserializer;
 import org.apache.hyracks.api.exceptions.HyracksDataException;
 import org.apache.hyracks.data.std.api.IPointable;
 import org.apache.hyracks.data.std.primitive.VoidPointable;
@@ -66,20 +69,24 @@ public abstract class AbstractSTGeometryDoubleNDescriptor extends AbstractScalar
 
         private ArrayBackedValueStorage resultStorage;
         private DataOutput out;
-        private IPointable inputArg;
-        private IScalarEvaluator eval0;
         private IPointable inputArg0;
+        private IScalarEvaluator eval0;
+        private IPointable inputArg1;
         private IScalarEvaluator eval1;
         private ByteArrayAccessibleInputStream inStream;
         private DataInputStream dataIn;
+
+        @SuppressWarnings("unchecked")
+        private final ISerializerDeserializer<Object> rectangleSerde =
+            SerializerDeserializerProvider.INSTANCE.getSerializerDeserializer(BuiltinType.ARECTANGLE);
 
         public AbstractSTGeometryDoubleNEvaluator(IScalarEvaluatorFactory[] args, IEvaluatorContext ctx)
                 throws HyracksDataException {
             resultStorage = new ArrayBackedValueStorage();
             out = resultStorage.getDataOutput();
-            inputArg = new VoidPointable();
-            eval0 = args[0].createScalarEvaluator(ctx);
             inputArg0 = new VoidPointable();
+            eval0 = args[0].createScalarEvaluator(ctx);
+            inputArg1 = new VoidPointable();
             eval1 = args[1].createScalarEvaluator(ctx);
             inStream = new ByteArrayAccessibleInputStream(new byte[0], 0, 0);
             dataIn = new DataInputStream(inStream);
@@ -89,36 +96,40 @@ public abstract class AbstractSTGeometryDoubleNDescriptor extends AbstractScalar
         public void evaluate(IFrameTupleReference tuple, IPointable result) throws HyracksDataException {
             resultStorage.reset();
 
-            eval0.evaluate(tuple, inputArg);
-            byte[] data0 = inputArg.getByteArray();
-            int offset = inputArg.getStartOffset();
-            int len = inputArg.getLength();
+            eval0.evaluate(tuple, inputArg0);
+            eval1.evaluate(tuple, inputArg1);
 
-            eval1.evaluate(tuple, inputArg0);
-            byte[] data1 = inputArg0.getByteArray();
+            if (PointableHelper.checkAndSetMissingOrNull(result, inputArg0, inputArg1)) {
+                return;
+            }
+
+            byte[] data0 = inputArg0.getByteArray();
             int offset0 = inputArg0.getStartOffset();
+            int len = inputArg0.getLength();
 
-            if (data0[offset] != ATypeTag.SERIALIZED_GEOMETRY_TYPE_TAG) {
-                throw new TypeMismatchException(sourceLoc, getIdentifier(), 0, data0[offset],
+            byte[] data1 = inputArg1.getByteArray();
+            int offset1 = inputArg1.getStartOffset();
+
+            if (data0[offset0] != ATypeTag.SERIALIZED_GEOMETRY_TYPE_TAG) {
+                throw new TypeMismatchException(sourceLoc, getIdentifier(), 0, data0[offset0],
                         ATypeTag.SERIALIZED_GEOMETRY_TYPE_TAG);
             }
 
-            inStream.setContent(data0, offset + 1, len - 1);
+            inStream.setContent(data0, offset0 + 1, len - 1);
             OGCGeometry geometry = AGeometrySerializerDeserializer.INSTANCE.deserialize(dataIn).getGeometry();
             Object finalResult;
-            if (data1[offset0] == ATypeTag.SERIALIZED_DOUBLE_TYPE_TAG) {
+            if (data1[offset1] == ATypeTag.SERIALIZED_DOUBLE_TYPE_TAG) {
                 finalResult =
-                        evaluateOGCGeometry(geometry, ADoubleSerializerDeserializer.getDouble(data1, offset0 + 1));
-            } else if (data1[offset0] == ATypeTag.SERIALIZED_INT64_TYPE_TAG) {
-                finalResult = evaluateOGCGeometry(geometry, AInt64SerializerDeserializer.getLong(data1, offset0 + 1));
+                        evaluateOGCGeometry(geometry, ADoubleSerializerDeserializer.getDouble(data1, offset1 + 1));
+            } else if (data1[offset1] == ATypeTag.SERIALIZED_INT64_TYPE_TAG) {
+                finalResult = evaluateOGCGeometry(geometry, AInt64SerializerDeserializer.getLong(data1, offset1 + 1));
             } else {
-                throw new TypeMismatchException(sourceLoc, getIdentifier(), 0, data1[offset0],
+                throw new TypeMismatchException(sourceLoc, getIdentifier(), 0, data1[offset1],
                         ATypeTag.SERIALIZED_DOUBLE_TYPE_TAG);
             }
 
             try {
-                SerializerDeserializerProvider.INSTANCE.getSerializerDeserializer(BuiltinType.ARECTANGLE)
-                        .serialize(finalResult, out);
+                rectangleSerde.serialize(finalResult, out);
                 result.set(resultStorage);
             } catch (IOException e) {
                 throw new InvalidDataFormatException(sourceLoc, getIdentifier(), e,
